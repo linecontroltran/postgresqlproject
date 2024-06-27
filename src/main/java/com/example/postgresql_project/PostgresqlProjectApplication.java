7333848dd68878865e05bea05178d8af0feea4d9
Tom <linecontroltran@gmail.com>
2024-06-27 14:50:44 -0400

    Version 1

---
+++ src/main/java/com/example/postgresql_project/PostgresqlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresql_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postgresql_project/PostgresqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresql_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
